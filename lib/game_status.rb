adb6a1d1430701e18fc5bf0e84bda26db520302d
Jordan Reed <jordanreed03@gmail.com>
2021-01-23 03:37:56 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -29,10 +29,8 @@ def won?(board)
    
     if position_1 == position_2 && position_2 == position_3 && position_taken?(board, win_index_1)
       return single_win_combo
-    else 
-      return false
     end
-  end
+  end return false
 end
 
 def full?(board)  
